a41461c1b6afd70f231e6c7fe2a254887a26119d
Rajeev Balendra <rj.balendra@gmail.com>
2021-05-25 22:06:58 +1000

    JS basics - blackjack

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
-// 1. Create a variable called isAlive and assign it to true
 let isAlive = true
+let message = ""
 
-// 2. Flip its value to false in the appropriate code block 
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+function startGame(){
+    
+
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
     isAlive = false
 }
-
-// 3. Log it out to check that you're doing it right
-
-console.log(isAlive)
+console.log(message)
+}
