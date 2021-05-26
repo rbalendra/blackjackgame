711e217b76c41a57cedf4ded32893a20cfa433fd
Rajeev Balendra <rj.balendra@gmail.com>
2021-05-26 21:57:41 +1000

    queryselectorAll

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 let firstCard = 10
-let secondCard = 4
+let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
-function startGame(){
-    
+// 1. Store the message-el paragraph in a variable called messageEl
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
-}
-console.log(message)
+function startGame() {
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    // 2. Display the message in the messageEl using messageEl.textContent
+    messageEl.textContent = message
 }
