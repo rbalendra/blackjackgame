92e1fbb7806bdedfe62ab5090ac0341482e0f11f
Rajeev Balendra <rj.balendra@gmail.com>
2021-05-30 00:23:24 +1000

    Assigning values to startgame function

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-let firstCard = 10
-let secondCard = 4
+
 // 1. Create a new array - cards - that contains firstCard and secondCard
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+     // if 1     -> return 11
+    // if 11-13 -> return 10
+    let randomNumber = Math.floor( Math.random()*13 ) + 1 // 1-13
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    // Generate two random numbes
+    // Re-assign the cards and sum variables so that the game can start
     renderGame()
 }
 
@@ -37,7 +59,7 @@ function renderGame() {
 
 
 function newCard() {
-    let card = 6
+    let card = getRandomCard()
     sum += card
     cards.push(card)
     renderGame()
